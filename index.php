9d146f9e49f50aa688a9150507e44585883e47f6
semen <semen_proo00@mail.ru>
2026-02-18 21:55:31 +0700

    Добавить поле: Year of registration

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
     <p>Address</p>
     <input type="text" name="Address">
     
+    <!-- Добавьте эти две строки -->
+    <p>Year of registration</p>
+    <input type="number" name="Year_of_registration">
+    
     <button type="submit">Add</button>
 </form>
