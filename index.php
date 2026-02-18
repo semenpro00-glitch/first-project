91de49a7ecb923451f33c593e5689a5abd99db11
FastickRND <semen_pro00@mail.ru>
2026-02-18 21:48:18 +0700

    Year of registration
    добавили поле:Year of registration

---
+++ index.php
@@ -0,0 +1,16 @@
+<h2>Add a new plot</h2>
+<form action="vendor/create.php" method="post">
+    <p>Owner</p>
+    <input type="text" name="Owner">
+    
+    <p>Square</p>
+    <input type="number" name="Square">
+    
+    <p>Address</p>
+    <input type="text" name="Address">
+    
+    <p>Year of registration</p>
+    <input type="number" name="Year">
+    
+    <button type="submit">Add</button>
+</form>
