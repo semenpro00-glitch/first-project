30b529639f5f815cc7ad61d32a8ada2d2efd33c7
semen <semen_proo00@mail.ru>
2026-02-18 21:44:51 +0700

    поле: year of registration

---
+++ index.php
@@ -0,0 +1,13 @@
+<h2>Add a new plot</h2>
+<form action="vendor/create.php" method="post">
+    <p>Owner</p>
+    <input type="text" name="Owner">
+    
+    <p>Square</p>
+    <input type="number" name="Square">
+    
+    <p>Address</p>
+    <input type="text" name="Address">
+    
+    <button type="submit">Add</button>
+</form>
